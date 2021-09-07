147213ef91af31c32852a092a7d29ddf75d11213
Wojciech Siewierski <wojciech@siewierski.eu>
2021-09-07 17:37:36 +0200

    Nix: Fetch the current master

--- default.nix
+++ default.nix
@@ -4,9 +4,10 @@ pkgs.buildGoPackage rec {
   pname = "nixos-fzf";
   version = "0.9";
   goPackagePath = "gitlab.com/wsiewierski/nixos-fzf";
-  src = builtins.filterSource
-    (path: type: type != "directory" || baseNameOf path != ".git")
-    ./.;
+  src = fetchGit {
+    url = "https://${goPackagePath}";
+    ref = "master";
+  };
   buildInputs = with pkgs; [
     makeWrapper
     fzf less
